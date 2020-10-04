2552c3526a6198e133f797f40f9b0e7d41c80f0c
Dennis Chacko <dennis.x.chacko@gmail.com>
2020-10-04 13:33:22 -0500

    initial version

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { FullscreenOverlayContainer } from '@angular/cdk/overlay';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'splash' },
+  { path: 'splash', component: SplashScreenComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+
+@NgModule({
+  declarations: [AppComponent, SplashScreenComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class AppModule {}
