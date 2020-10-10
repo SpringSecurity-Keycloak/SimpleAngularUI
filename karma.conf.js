3c5fd8275fba7a4a537396b1475267709621c2a2
Dennis Chacko <dennis.x.chacko@gmail.com>
2020-10-10 04:55:03 -0500

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -37,4 +37,6 @@ module.exports = function (config) {
       },
     },
   });
+  process.env.NO_PROXY = "localhost, 0.0.0.0/4201, 0.0.0.0/9876";
+  process.env.no_proxy = "localhost, 0.0.0.0/4201, 0.0.0.0/9876";
 };
