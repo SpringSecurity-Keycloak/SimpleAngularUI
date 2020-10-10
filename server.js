8e9acd556f58a6d91dcfea64f6ec7e955ecfb2e2
Dennis Chacko <dennis.x.chacko@gmail.com>
2020-10-10 17:22:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/dchacko-simple-angular-ui"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile(
+    path.join(__dirname + "/dist/dchacko-simple-angular-ui/index.html")
+  );
 });
 
 // Start the app by listening on the default Heroku port
